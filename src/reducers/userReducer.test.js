bad2ccb5c3286c3008425404ef7f3d2f2509af06
AaronMullan <aaroncmullan@gmail.com>
2020-02-14 15:49:06 -0800

    userReducer test passing

--- src/reducers/userReducer.test.js
+++ src/reducers/userReducer.test.js
@@ -1,13 +1,22 @@
 import { FETCH_USER } from '../actions/userActions';
 import reducer from './userReducer';
 
-describe('repos reducer', () => {
-  it('handles the fetch repos action', () => {
+describe('user reducer', () => {
+  it('handles the fetch user action', () => {
     const action = {
       type: FETCH_USER,
       payload: [
-        {}
+        { user: 'AaronMullan' }
       ]
     };
+    const initalState = { list: [] };
+
+    const newState = reducer(initalState, action);
+
+    expect(newState).toEqual({
+      list:  [
+        { user: 'AaronMullan' }
+      ] 
+    });
   });
 });
