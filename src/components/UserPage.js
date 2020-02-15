d7e1c0f4bb1823f811942853ea09e94003fea852
AaronMullan <aaroncmullan@gmail.com>
2020-02-14 16:36:25 -0800

    annoying that travis is on my case

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUser } from '../actions/userActions';
 
@@ -6,8 +6,17 @@ const UserPage = () => {
   const dispatch = useDispatch();
   const posts = useSelector(fetchUser);
 
+  const [text, setText] = useState();
+
   return (
-    <h1>greetings from UserPage</h1>
+    <>
+      <h1>Github Displayer</h1>
+      <form>
+        <h2>Input Github Username</h2>
+        <input type="text" value= { text }/>
+        <button onSubmit={setText}>Submit</button>
+      </form>
+    </>
   );
 
 };
