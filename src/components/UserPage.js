5abeea49f1242c7901360a70371e3316f1556c89
AaronMullan <aaroncmullan@gmail.com>
2020-02-14 17:33:39 -0800

    userSelector test passing

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -6,15 +6,15 @@ const UserPage = () => {
   const dispatch = useDispatch();
   const posts = useSelector(fetchUser);
 
-  const [text, setText] = useState();
+  const [text, setText] = useState('');
 
   return (
     <>
       <h1>Github Displayer</h1>
       <form>
         <h2>Input Github Username</h2>
-        <input type="text" value= { text }/>
-        <button onSubmit={setText}>Submit</button>
+        <input type="text" value= {text} onChange={({ target }) => setText(target.value)}/>
+        <button>Submit</button>
       </form>
     </>
   );
